cd4b83cee00e2b7f2e88e9a8acec24f7b6c0acd6
shangshikun <12606892+heheshang@users.noreply.github.com>
2018-12-29 17:42:28 +0800

    修改代码结构

--- concurrency-demo/src/test/java/com/wangwenjun/concurrency/book27/future/ActiveFuture.java
+++ concurrency-demo/src/test/java/com/wangwenjun/concurrency/book27/future/ActiveFuture.java
@@ -12,7 +12,7 @@ import com.wangwenjun.concurrency.book19.FutureTask;
 public class ActiveFuture<T> extends FutureTask<T> {
 
     @Override
-    protected void finish(T result) {
+    public void finish(T result) {
 
         super.finish(result);
     }
